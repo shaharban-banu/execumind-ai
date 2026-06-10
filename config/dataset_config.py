40d25e48d83fe855fe34de467544d317aaaf881a
shaharban-banu <sonushaharban44@gmail.com>
2026-06-10 23:01:55 +0530

    Initialize ExecuMind AI project structure

---
+++ config/dataset_config.py
@@ -0,0 +1,11 @@
+DATASETS={
+    "customers":"olist_customers_dataset.csv",
+    "orders": "olist_orders_dataset.csv",
+    "order_items": "olist_order_items_dataset.csv",
+    "payments": "olist_order_payments_dataset.csv",
+    "products": "olist_products_dataset.csv",
+    "reviews": "olist_order_reviews_dataset.csv",
+    "sellers": "olist_sellers_dataset.csv",
+    "geolocation": "olist_geolocation_dataset.csv"
+
+}
